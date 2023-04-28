c1c676296e3e659c3fa3c7df687b87ca0d43f83c
Adetops <heritagediwamu@gmail.com>
2023-04-28 08:04:34 -0700

    Other string functions

---
+++ 2-str_funcs.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * startWith - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ * Return: address of next char of haystact or NULL
+ */
+char *startWith(const char *haystack, const char *needle)
+{
+	while (*needle)
+	{
+		if (*needle++ != *haystack++)
+			return (NULL);
+	}
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: pointer to dest buff
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: first input string
+ * @s2: second input string
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, res = 0;
+
+	while (res == 0)
+	{
+		if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+			break;
+		res = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (res);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src including the terminating '\0'
+ * to the buffer pointed to by dest
+ * @dest: the destination string
+ * @src: the source string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
